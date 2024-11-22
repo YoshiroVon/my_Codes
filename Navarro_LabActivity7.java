a7eabb27e7e29118c6c5039bc338259bcb442d78
Josh_Navarro <joshnavarro123693l@gmail.com>
2024-11-22 20:57:31 +0800

    o

---
+++ Navarro_LabActivity7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Navarro_LabActivity7
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result;
+        System.out.print("Print Squares of Numbers\n Input Number: ");
+        int input = sc.nextInt();
+
+        for (int i = 1; i <= input; i++) {
+            result = i * i;
+
+            System.out.printf("%d⌃%d=%d%n",i ,i ,result);
+        }
+        
+    }
+}
