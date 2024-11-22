544cd9a3d28eb7bc36a948f991964cba3c8fe7a8
Josh_Navarro <joshnavarro123693l@gmail.com>
2024-11-22 23:07:17 +0800

    o

--- Navarro_523_Activity6_6.java
+++ Navarro_523_Activity6_6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
-public class Navarro_Activity {
+public class Navarro_523_Activity6_6 {
 
     /**
      * @param args the command line arguments
